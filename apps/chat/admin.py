51b7d495bc8091c05d267c17ed1a304cf2ae9ee8
nadeali0999 <nadeali0999@gmail.com>
2024-06-11 15:58:53 +0500

    this quizzes changes

--- apps/chat/admin.py
+++ apps/chat/admin.py
@@ -4,4 +4,4 @@ from .models import UserProfile, Messages, Friends
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Messages)
-admin.site.register(Friends)
+# admin.site.register(Friends)
