27d0d25ed80ebcc7363b907e5546fa119b82443e
nadeali0999 <nadeali0999@gmail.com>
2024-06-28 16:29:02 +0500

    this is latest db changes

--- core/settings.py
+++ core/settings.py
@@ -80,18 +80,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'lms',
-    #     'USER': 'myuser',
-    #     'PASSWORD': 'mypass',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / "db.sqlite3",
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myfyp',
+        'USER': 'myfypuser',
+        'PASSWORD': 'mypass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 }
 
 # Password validation
